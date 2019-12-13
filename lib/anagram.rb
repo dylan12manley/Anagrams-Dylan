d99e4a8f9827b07fd38700dede7b1994f2562d19
Dylan Manley <dylan.manley@gmail.com>
2019-12-13 12:57:07 -0800

    create word_finder method, and make seccond test pass

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,22 +6,27 @@ class Anagram
     @word_two = word_two
     @vowels =  ["a", "e", "i", "o", "u", "y"]
     @return_statement = ""
+    @are_words = ""
   end
 
   def anagram_finder
-    sorted_word_one = @word_one.downcase.split('').sort
-    sorted_word_two = @word_two.downcase.split('').sort
-    @word_one = sorted_word_one
-    @word_two = sorted_word_two
-    sorted_word_one.each do |letter|
+    @word_one = @word_one.downcase.split('').sort
+    @word_two = @word_two.downcase.split('').sort
+    if @word_one == @word_two
+      @return_statement = "These words are anagrams."
+    else
+      @return_statement = "These words are not anagrams."
+    end
+    @return_statement
+  end
+
+  def word_finder
+    @word_one = @word_one.downcase.split('').sort
+    @word_two = @word_two.downcase.split('').sort
+    @word_one.each do |letter|
       if @vowels.include?(letter)
-        sorted_word_two.each do |letter|
+        @word_two.each do |letter|
           if @vowels.include?(letter)
-            if @word_one == @word_two
-              @return_statement = "These words are anagrams."
-            else
-              @return_statement = "These words are not anagrams."
-            end
           else
             @return_statement = "These do not contain vowells, and aren't words."
           end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -2,12 +2,12 @@ require('rspec')
 require('anagram')
 
 describe('#anagram_finder') do
-  it('checks if two words are anagrams') do
+  it('checks if Pants and spant are anagrams') do
     anagram = Anagram.new("PANTS", "spant")
     expect(anagram.anagram_finder()).to(eq("These words are anagrams."))
   end
-  # it('checks if words are words') do
-  #   anagram = Anagram.new("pfff", "fpff")
-  #   expect(anagram.anagram_finder()).to(eq("These do not contain vowells, and aren't words."))
-  # end
+  it('checks if words are words') do
+    anagram = Anagram.new("pfff", "fpff")
+    expect(anagram.word_finder()).to(eq("These do not contain vowells, and aren't words."))
+  end
 end
