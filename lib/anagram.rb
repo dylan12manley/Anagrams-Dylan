540febe982f2489ce5afff112de85afca69c36a0
Dylan Manley <dylan.manley@gmail.com>
2019-12-13 16:48:23 -0800

    refactor so word finder isnt a seperate method, but didn't finish, commented out changes

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,45 +6,39 @@ class Anagram
     @word_two = word_two
     @vowels =  ["a", "e", "i", "o", "u", "y"]
     @return_statement = ""
-  end
-
-  def anagram_finder
-    @word_one = @word_one.downcase.split('').sort
-    @word_two = @word_two.downcase.split('').sort
-    first_word = @word_one
-    seccond_word = @word_two
-    puts first_word
-    puts seccond_word
-    if first_word == seccond_word
-      @return_statement = "These words are anagrams."
-    else
-    first_word.each do |letter|
-      if seccond_word.include?(letter)
-        @return_statement = "These words are not anagrams."
+    
+    def anagram_finder
+      @word_one = @word_one.downcase.split('').sort
+      @word_two = @word_two.downcase.split('').sort
+      first_word = @word_one
+      seccond_word = @word_two
+      # word finder
+      # (/W[aeiou]rd/.match(@word_one.to_s)) == true || (/W[aeiou]rd/.match(@word_two.to_s) == true)
+      # if first_word.include?(@vowels) != true || seccond_word.include?(@vowels).include? != true
+      #   @return_statement = "These do not contain vowells, and aren't words."
+      # else
+      # end
+      if first_word == seccond_word
+        @return_statement = "These words are anagrams."
       else
-        @return_statement = "These are antigrams."
+        first_word.each do |letter|
+          if seccond_word.include?(letter)
+            @return_statement = "These words are not anagrams."
+          else
+            @return_statement = "These are antigrams."
+          end
+        end
       end
+      @return_statement
     end
-  end
-# word finder
-    # @word_one.each do |letter|
-    #   if @vowels.excludes?(letter)
-    #     @return_statement = "These do not contain vowells, and aren't words."
-    #   end
-    # end
-    # @word_two.each do |letter|
-    #   if @vowels.excludes?(letter)
-    #     @return_statement = "These do not contain vowells, and aren't words."
-    #   end
-    # end
-    @return_statement
   end
 
   def word_finder
     @word_one = @word_one.downcase.split('').sort
     @word_two = @word_two.downcase.split('').sort
     @word_one.each do |letter|
-      if @vowels.include?(letter)
+      if /W[aeiou]rd/.match(@word_one.to_s) && /W[aeiou]rd/.match(@word_2.to_s)
+        # if @vowels.include?(letter)
         @word_two.each do |letter|
           if @vowels.include?(letter)
           else
@@ -53,19 +47,6 @@ class Anagram
         end
       else
         @return_statement = "These do not contain vowells, and aren't words."
-      end
-    end
-    @return_statement
-  end
-
-  def antigram_finder
-    @word_one = @word_one.downcase.split('').sort
-    @word_two = @word_two.downcase.split('').sort
-    @word_one.each do |letter|
-      if @word_two.include?(letter)
-        @return_statement = "These are not antigrams."
-      else
-        @return_statement = "These are antigrams."
       end
     end
     @return_statement
