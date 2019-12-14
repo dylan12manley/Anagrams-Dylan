dc5af94005e8e42a7c0d0791cd73641e510518c9
Dylan Manley <dylan.manley@gmail.com>
2019-12-13 16:59:11 -0800

    add .delete method back

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,18 +6,12 @@ class Anagram
     @word_two = word_two
     @vowels =  ["a", "e", "i", "o", "u", "y"]
     @return_statement = ""
-    
+
     def anagram_finder
-      @word_one = @word_one.downcase.split('').sort
-      @word_two = @word_two.downcase.split('').sort
+      @word_one = @word_one.downcase.split('').sort.delete("")
+      @word_two = @word_two.downcase.split('').sort.delete("")
       first_word = @word_one
       seccond_word = @word_two
-      # word finder
-      # (/W[aeiou]rd/.match(@word_one.to_s)) == true || (/W[aeiou]rd/.match(@word_two.to_s) == true)
-      # if first_word.include?(@vowels) != true || seccond_word.include?(@vowels).include? != true
-      #   @return_statement = "These do not contain vowells, and aren't words."
-      # else
-      # end
       if first_word == seccond_word
         @return_statement = "These words are anagrams."
       else
@@ -32,6 +26,12 @@ class Anagram
       @return_statement
     end
   end
+  # word finder
+  # (/W[aeiou]rd/.match(@word_one.to_s)) == true || (/W[aeiou]rd/.match(@word_two.to_s) == true)
+  # if first_word.include?(@vowels) != true || seccond_word.include?(@vowels).include? != true
+  #   @return_statement = "These do not contain vowells, and aren't words."
+  # else
+  # end
 
   def word_finder
     @word_one = @word_one.downcase.split('').sort
