19d73cc66e3a0238b747c6822af6431ae7c41def
Dylan Manley <dylan.manley@gmail.com>
2019-12-17 20:38:29 -0800

    refactor so gsub, split and downcase methods are called in the initalisation of Anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,14 @@
 class Anagram
-  attr_accessor(:word_one, :word_two)
 
   def initialize(word_one, word_two)
     @word_one = word_one
     @word_two = word_two
-    @vowels =  ["a", "e", "i", "o", "u", "y"]
+    @word_one = @word_one.gsub(/[\s[:punct:]]/, '').downcase.split('').sort
+    @word_two = @word_two.gsub(/[\s[:punct:]]/, '').downcase.split('').sort
     @return_statement = ""
 
     def anagram_finder
-      @word_one = @word_one.gsub(/[\s[:punct:]]/, '').downcase.split('').sort
-      @word_two = @word_two.gsub(/[\s[:punct:]]/, '').downcase.split('').sort
-      puts @word_one
-      puts @word_two
-      first_word = @word_one
-      seccond_word = @word_two
-      if first_word == seccond_word
+      if @word_one == @word_two
         @return_statement = "These words are anagrams."
       else
         @return_statement = "These words not anagrams."
@@ -24,8 +18,6 @@ class Anagram
   end
 
   def antigram_finder
-    @word_one = @word_one.downcase.split('').sort
-    @word_two = @word_two.downcase.split('').sort
     @word_one.each do |letter|
       if @word_two.include?(letter)
         @return_statement = "These are not antigrams."
@@ -38,11 +30,8 @@ class Anagram
 
 
   def word_finder
-    @word_one = @word_one.downcase.split('').sort
-    @word_two = @word_two.downcase.split('').sort
     @word_one.each do |letter|
       if /W[aeiou]rd/.match(@word_one.to_s) && /W[aeiou]rd/.match(@word_2.to_s)
-        # if @vowels.include?(letter)
         @word_two.each do |letter|
           if @vowels.include?(letter)
           else
